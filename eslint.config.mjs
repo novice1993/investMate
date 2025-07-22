05422b9fcfd40151498704c2f93fba1233b208e4
kimyh93 <kimyh93@rexgen.co.kr>
2025-07-22 18:42:45 +0900

    feat: 개발 환경 셋팅
    Desc
    - next + ts + tailwind
    - prettier, eslint, husky, lint-staged 설치 및 관련 config 정리

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    ignores: ['node_modules', '.next', 'dist'],
+    plugins: {
+      import: eslintPluginImport,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': ['error'],
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'always',
+        },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
