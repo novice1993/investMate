42ff0c698c1511cf78ef9dca07749c2cb1cec590
kimyh93 <kimyh93@rexgen.co.kr>
2025-07-22 18:59:16 +0900

    feat: 테스팅 라이브러리 추가 (vitest)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import eslintPluginImport from 'eslint-plugin-import';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginImport from "eslint-plugin-import";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,22 +12,22 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    files: ['**/*.{js,ts,jsx,tsx}'],
-    ignores: ['node_modules', '.next', 'dist'],
+    files: ["**/*.{js,ts,jsx,tsx}"],
+    ignores: ["node_modules", ".next", "dist"],
     plugins: {
       import: eslintPluginImport,
       prettier: eslintPluginPrettier,
     },
     rules: {
-      'prettier/prettier': ['error'],
-      'import/order': [
-        'warn',
+      "prettier/prettier": ["error"],
+      "import/order": [
+        "warn",
         {
-          groups: ['builtin', 'external', 'internal'],
-          alphabetize: { order: 'asc', caseInsensitive: true },
-          'newlines-between': 'always',
+          groups: ["builtin", "external", "internal"],
+          alphabetize: { order: "asc", caseInsensitive: true },
+          "newlines-between": "always",
         },
       ],
     },
